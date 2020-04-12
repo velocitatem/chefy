9b35f8c84ad78b9116665b78c7a26f8b8e0e9abd
Daniel Rosel <danalves24@outlook.com>
2020-04-12 18:49:25 +0200

    length bug fix

--- src/Elements/Book.js
+++ src/Elements/Book.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import $ from  'jquery';
 var rcp
+var dataLength = 0
+
 fetch("https://danalves24com.github.io/data/cookbook-api/api.json")
 .then((response) => {
     return response.json();
@@ -8,6 +10,9 @@ fetch("https://danalves24com.github.io/data/cookbook-api/api.json")
 .then((data) => {
     console.log(data)
     rcp = data
+    for (var dt in rcp[0]) {
+        dataLength++
+    }
     var filterTags = []
 var output
 var book = rcp[0]
@@ -18,7 +23,9 @@ console.log(rcp)
 function ERRnotFound() {
 notFound+=1
 console.log(notFound)
-if (notFound == rcp[0].length){
+
+
+if (notFound == dataLength){
 $("#items").html(`
 <center>
 <h2>Sorry... We couldn't find anything yet 🙁
@@ -190,15 +197,14 @@ function Book() {
 }
 /*
 "1": {
-    "name": "",
-    "resources": [""],
-    "tags": [],
-    "steps": {
-        "ingredients": [""],
-        "bakingSteps": {
-            "step1" : "",
+        "name": "",
+        "resources": [""],
+        "steps": {
+            "ingredients": [""],
+            "bakingSteps": {
+                "step1" : "",
+            }
         }
     }
-}
 */
 export default Book;
