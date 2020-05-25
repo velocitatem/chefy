bda95aa5a6f40a6f2413b84820d1e88dbf95deb0
Daniel Rosel <danalves24@outlook.com>
2020-05-25 13:01:01 +0200

    unit conversion algorithm and fronted imp

--- src/Elements/Book.js
+++ src/Elements/Book.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import $ from 'jquery';
+import ModalMetric from './metric'
+import modalMetric from './metric';
+
 var rcp
 var dataLength = 0
-
+var initialUnit = "metric"
 function report(abt) {
     const headers = new Headers()
     headers.append("Content-Type", "application/json")
@@ -80,6 +83,7 @@ function match(steps, allSteps, book, r, step) {
             filterTags.push(book[r].tags[t])
         }
     }        
+    
     $("#items").append(`
     <p>    
     <div class="row">
@@ -98,7 +102,9 @@ function match(steps, allSteps, book, r, step) {
         <a id="tags">
         ${tags} 
         </a> <br>
-        <a href="/share/?item=${book[r].name}">Share/Enlarge</a> 
+        <a href="/share/?item=${book[r].name}">Share/Enlarge</a> <br>
+        <button class="btn btn-outline-dark" data-toggle="modal" data-target="#metricModal">Metric</button>
+        <button class="btn btn-outline-dark" id='imperial' data-toggle="modal" data-target="#imperialModal">Imperial</button>                
         <h5>Ingredients: </h5> 
         <ul id='ings'>
         ${ing}
@@ -197,7 +203,7 @@ $(document).ready(function () {
             ${tags} 
             </a>           
             <br>            
-            <a href="/share/?item=${book[r].name}">Share/Enlarge</a> 
+            <a href="/share/?item=${book[r].name}">Share/Enlarge</a> <br>
             <p id="missing">      
             ${missingItems}      
             </p>
@@ -238,14 +244,227 @@ $(document).ready(function(){
       });
     });
   });
+
+/**             
+ */
+
+function convertImp() {
+    let amt = $("#valImp").val()
+    let unt = $("#unitImp").val()
+    console.log("converting", amt, unt)
+    let res
+    switch(unt){
+        case "cup/s":
+            res = amt*284.131
+            res = String(res)
+            res = res.substring(0, 4)
+            console.log(res, "ml/grams")
+            $("#metRes").html(`${res} ml/grams`)
+            break;
+        case "tbsp":
+            res = amt*17.7582
+            res = String(res)
+            res = res.substring(0, 4)
+            console.log(res, "ml/grams")
+            $("#metRes").html(`${res} ml/grams`)
+            break;
+        case "tsp":
+            res = amt*5.91939
+            res = String(res)
+            res = res.substring(0, 4)
+            console.log(res, "ml/grams")
+            $("#metRes").html(`${res} ml/grams`)
+            break;
+        case "inches":
+            res = amt*2.54
+            res = String(res)
+            res = res.substring(0, 4)
+            console.log(res, "cm")
+            $("#metRes").html(`${res} cm`)
+        case "ounce/s":
+            res = amt*28.34952
+            res = String(res)
+            res = res.substring(0, 4)
+            console.log(res, "grams/ml")
+            $("#metRes").html(`${res} grams/ml`)
+            break;
+        case 'pound/s':
+            res = amt*453.5924
+            res = String(res)
+            res = res.substring(0, 4)
+            console.log(res, "grams/ml")
+            $("#metRes").html(`${res} grams/ml`)
+            break;
+    }
+}
+
+function convertMet() {    
+    let amt = parseInt($("#valMet").val())
+    let unt = $("#unitMet").val()
+    console.log("converting", amt, unt)
+    let res
+    switch(unt){
+    case "gram/s":
+        if (amt<=450) {
+            res = amt*0.035
+            res = String(res)
+            res = res.substring(0, 4)
+            console.log(res, "oz")
+            $("#impRes").html(`${res} oz`)
+        }
+        else if(amt>=450) {
+            res = amt*0.0022
+            res = String(res)
+            res = res.substring(0, 4)
+            console.log(res, "lb")
+            $("#impRes").html(`${res} lb`)
+
+        }
+        break;
+    case "mililiter/s":
+        if(amt<=5){ 
+            res = 0.2*amt
+            res = String(res)
+            res = res.substring(0, 3)
+            console.log(res, "tsp")
+            $("#impRes").html(`${res} tsp`)
+        }
+        else if(amt<=15 && amt>5) {
+            res = 0.05631201*amt
+            res = String(res)
+            res = res.substring(0, 3)
+            console.log(res, "tbsp")
+            $("#impRes").html(`${res} tbsp`)
+        }
+        else if(amt>=60){
+            res = 0.003519503*amt
+            res = String(res)
+            res = res.substring(0, 3)
+            console.log(res, "tbsp")
+            $("#impRes").html(`${res} cup/s`)
+        }
+        break;
+    case "centimeter/s":
+        if(amt<30){
+            res = amt*0.3937008
+            res = String(res)
+            res = res.substring(0, 3)
+            console.log(res, "tbsp")
+            $("#impRes").html(`${res} in`)
+        }
+        else if(amt>=30){
+            res = amt*0.03280839895
+            res = String(res)
+            res = res.substring(0, 3)
+            console.log(res, "tbsp")
+            $("#impRes").html(`${res} feet`)
+        }
+        break;
+}
+}
+
+
 function Book() {
   return (
     <div class="wrapper">
-      <div class="container" >        
+      <div class="container" >       
+
+
+  <div class="modal" id="metricModal">
+    <div class="modal-dialog">
+      <div class="modal-content">
+      
+        <div class="modal-header">
+          <h4 class="modal-title">Metric => Imperial</h4>
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+        </div>
+        
+        <div class="modal-body">
+
+
+        <div class="input-group"> 
+        <input type="number" id="valMet" class="form-control"></input>
+
+  <div class="input-group-append">
+  <select id="unitMet" class="form-control">
+            <option class="form-control">---</option>
+            <option class="form-control">gram/s</option>
+            <option class="form-control">mililiter/s</option>
+            <option class="form-control">centimeter/s</option>
+        </select>
+        <button class="form-control" onClick={convertMet}>Convert</button>
+  </div>
+</div>
+
+
+
+        <br></br>
+        =
+        <span id="impRes"></span>
+
+        </div>
+        
+        <div class="modal-footer">
+          <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+        </div>
+        
+      </div>
+    </div>
+  </div>
+
+  <div class="modal" id="imperialModal">
+    <div class="modal-dialog">
+      <div class="modal-content">
+      
+        <div class="modal-header">
+          <h4 class="modal-title">Imperial => Metric</h4>
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+        </div>
+        
+        <div class="modal-body">
+
+        <div class="input-group">
+        <input pattern="^[0-9/]*$" id="valImp" class="form-control"></input>
+
+  <div class="input-group-append">
+  <select id="unitImp" class="form-control">
+            <option>---</option>
+            <option>cup/s</option>
+            <option>tsp</option>
+            <option>tbsp</option>
+            <option>inches</option>
+            <option>ounce/s</option>
+            <option>pound/s</option>
+        </select> 
+        <button onClick={convertImp} class="form-control">Convert</button>
+  </div>
+</div>
+
+
+        <br></br>
+        =
+        <span id="metRes"></span>
+
+        </div>
+        
+        <div class="modal-footer">
+          <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+        </div>
+        
+      </div>
+    </div>
+  </div>
+
         <div class="row">
+
             <div id="filterW">
-            <div class="panel panel-default">
-                <div class="panel-body"><input id="filter" list="filterO" placeholder="select category.."></input>  </div>
+            <div>
+                <div ><input id="filter" list="filterO"  class="form-control" placeholder="select category.."></input>
+                <br></br>
+                <div id='units'>
+                    
+                </div>
+                </div>
             </div>                  
                 <datalist id="filterO">                
                 </datalist> 
---
+++ src/Elements/metric.js
@@ -0,0 +1,13 @@
+import React from 'react'
+const modalMetric = () => (
+    <>
+        <input></input>
+        <select></select>
+        =>
+        <select></select> <br></br>
+        =
+        <span></span>
+    </>
+)
+
+export default modalMetric;
